b7dabeac59efe4e6859282d8190a8a4eb3de719b
EgorNikitin1 <90258854+EgorNikitin1@users.noreply.github.com>
2023-03-12 01:15:55 +0300

    First commit

--- 6exyPerez.py
+++ 6exyPerez.py
@@ -7,45 +7,41 @@ example = np.array([[1, 1, 1, -1, 1, -1, 1, 1, 1, -1, -1, 1, 1, -1, -1, 1],
 w = np.dot(x.T, x)
 for i in range(len(w)):
     w[i][i] = 0
-print("Синоптические коэффициенты:")
+print("Матрица весовых коэффициентов:")
 print(w)
 
-y = example[0]
-
-while True:
-    s = np.dot(w, y)
-    for i in range(len(s)):
-        if s[i] < 0:
-            s[i] = -1
-        else:
-            s[i] = 1
-    print(s)
-    print(sum((y - s) ** 2))
-    if sum((y-s)**2) == 0:
-        break
-    y = s
-
-y = example[1]
-
-temp = []
-while True:
-    s = np.dot(w, y)
-    for i in range(len(s)):
-        if s[i] < 0:
-            s[i] = -1
-        else:
-            s[i] = 1
-
-    temp.append(s)
-
-    print(*temp)
-    if len(temp) == 3:
-        print(sum((y - temp[1]) ** 2), sum((s - temp[2]) ** 2))
-        if sum((y - temp[1]) ** 2) == 0 and sum((s - temp[2]) ** 2) == 0:
+for i in range(len(example)):
+    y = example[i]
+    print("Образ", i+1+len(x))
+    print("Входной вектор:")
+    print(y)
+    temp = []
+    count = 0
+    while True:
+        count += 1
+        s = np.dot(w, y)
+        for j in range(len(s)):
+            if s[j] < 0:
+                s[j] = -1
+            else:
+                s[j] = 1
+        temp.append(s)
+        print("Выходной вектор:")
+        for j in range(len(temp)):
+            print(temp[j])
+        print("Первое условие:", sum((y - s) ** 2))
+        if sum((y - s) ** 2) == 0:
+            for j in range(len(x)):
+                if temp[-1].tolist() == x[j].tolist():
+                    print("Образ инициализирован и соответствует эталонному образу", j+1)
             break
-        del temp[0]
-
-    if sum((y - s) ** 2) == 0:
-        print(sum((y - s) ** 2))
-        break
-    y = s
+        if len(temp) >= 2:
+            print("Второе условие:", sum((y - temp[1]) ** 2))
+        if len(temp) == 3:
+            print("Третье условие:", sum((s - temp[2]) ** 2))
+            if sum((y - temp[1]) ** 2) == 0 and sum((s - temp[2]) ** 2) == 0:
+                print("Образ неинициализирован")
+                break
+            del temp[0]
+        y = s
+    print("Количество итераций:", count)
